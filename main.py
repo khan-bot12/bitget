22fdd07dc302e325dfd020590d3a6cbc9e5562f6
khan-bot12 <mktraders.pvt@gmail.com>
2025-07-13 01:16:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
+import uvicorn
 from fastapi import FastAPI, Request
+import json
 from bitget_trade import smart_trade
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"msg": "✅ Bitget Smart Bot is running."}
-
 @app.post("/webhook")
 async def webhook(request: Request):
     data = await request.json()
-    print(f"📬 Incoming webhook: {data}")
-
+    print(f"📥 ALERT RECEIVED: {json.dumps(data)}")
     action = data.get("action")
     symbol = data.get("symbol")
     quantity = data.get("quantity")
-    leverage = data.get("leverage", 50)
+    leverage = data.get("leverage")
+    return smart_trade(action, symbol, quantity, leverage)
 
-    result = smart_trade(action, symbol, quantity, leverage)
-    return {"status": "ok", "result": result}
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=80)
